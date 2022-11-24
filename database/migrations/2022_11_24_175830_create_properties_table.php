982ce38631139c83a9c8be31ac00ce7c638d9fea
Mauro Vieira <mauro.emc@outlook.com>
2022-11-24 15:58:03 -0300

    dev#1 - Criação das migrations das tabelas property_types e properties

---
+++ database/migrations/2022_11_24_175830_create_properties_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')
+                  ->nullable();
+            $table->string('description')
+                  ->nullable();
+            $table->foreignId('property_type_id')
+                  ->references('id')
+                  ->on('property_types')
+                  ->nullable()
+                  ->onDelete('cascade');
+            $table->string('type');
+            $table->string('addr_cep')
+                  ->nullable();
+            $table->string('addr_number')
+                  ->nullable();
+            $table->string('complement')
+                  ->nullable();
+            $table->integer('bedrooms')
+                  ->nullable();
+            $table->integer('rooms')
+                  ->nullable();
+            $table->integer('garage')
+                  ->nullable();
+            $table->integer('pool')
+                  ->nullable();
+            $table->json('images')
+                  ->nullable();
+            $table->boolean('is_rented')
+                  ->nullable()
+                  ->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+};

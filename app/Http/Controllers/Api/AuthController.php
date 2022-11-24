3171b7bb70835a7339695ee6a31b554af7277f2e
Mauro Vieira <mauro.emc@outlook.com>
2022-11-24 14:36:15 -0300

    dev#1 - Implementado sistema de autenticação baseado em token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    const HTTP_CODE_UNAUTHORIZED = 401;
+    const HTTP_CODE_SUCCESS      = 200;
+
+    /**
+     * Create a new AuthController instance.
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $execResult  = [
+            'error'   => false,
+            'message' => ''
+        ];
+
+        $credentials = request(['email', 'password']);
+        $token       = auth()->attempt($credentials);
+
+        if (!$token)
+        {
+            $execResult['error']   = true;
+            $execResult['message'] = "Acesso não autorizado";
+
+            return response()->json(
+                                     $execResult,
+                                     self::HTTP_CODE_UNAUTHORIZED
+                                   );
+        }
+
+        $execResult['message']       = "Autenticação realizada com sucesso";
+        $execResult['Authorization'] = [
+                                         'type'  => 'bearer',
+                                         'token' => $token
+                                       ];
+
+        return response()->json(
+                                $execResult,
+                                self::HTTP_CODE_SUCCESS
+                               );
+    }
+
+    /**
+     * Get the authenticated User.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        $execResult = [
+                        'error'   => false,
+                        'message' => 'Usuário desconectado com sucesso'
+        ];
+
+        auth()->logout();
+
+        return response()->json($execResult);
+    }
+
+    /**
+     * Refresh a token.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+|--------------------------------------------------------------------------
+| Auth Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group([ 'middleware' => 'api', 'prefix' => 'auth' ], function ($router) {
+    Route::post('login',   [AuthController::class, 'login'])->name('login');
+    Route::get('login',    [AuthController::class, 'login'])->name('login');
+    Route::post('logout',  [AuthController::class, 'logout'])->name('api.logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('api.auth-refresh');
+    Route::post('me',      [AuthController::class, 'me'])->name('api.auth-me');
+
+});
+
